64bf62b3ab3634bae7f8c2d3b121458cf7bfece5
annmora13 <annmoracp@gmail.com>
2023-07-20 22:23:40 -0400

    Proyecto finalizado

--- test/test.js
+++ test/test.js
@@ -3,3 +3,16 @@ const chaiHttp = require('chai-http');
 const { app } = require('../index');
 chai.use(chaiHttp);
 
+describe('Testing REST API with Mocha - Chai', function(){
+    it('Testing GET - The data must contain a property called 1 and this must be an object', function(done){
+        chai
+            .request(app)
+            .get('/')
+            .end(function (err, res) {
+                let data = JSON.parse(res.text);
+                chai.expect(data).to.have.property('4');
+                chai.expect(data['4']).to.be.an('object');
+                done();
+            });
+    });
+});
